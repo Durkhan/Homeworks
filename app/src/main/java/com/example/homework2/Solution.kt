af8da54f08331fe8fd635b544bb98b5634a48f36
bumbumapp <bumbumapps@gmail.com>
2022-12-24 11:34:24 -0800

    launch coroutines in Async and Sync way

--- app/src/main/java/com/example/homework2/Solution.kt
+++ app/src/main/java/com/example/homework2/Solution.kt
@@ -1,16 +1,14 @@
 package com.example.homework2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
 class Solution : AppCompatActivity() {
-    private val list = mutableListOf<String>()
     private val state = MutableStateFlow("empty")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,71 +18,50 @@ class Solution : AppCompatActivity() {
 
     private fun main() {
         runSync()
-        runAsync()
+//        runAsync()
 
-
-
+        // subscribe to flow updates and print state.value to logcat.
         lifecycleScope.launch {
 //          observe state updates
             state.collect {
-                // subscribe to flow updates and print state.value to logcat.
-                if (it!="empty")
-                Log.d("Tag",it)
-
-
+                if (it != "empty")
+                    Log.d("Tag", it)
             }
         }
     }
 
     private fun runSync() {
         println("runSync method.")
-        //  launch 1000 coroutines. Invoke doWork(index/number of coroutine) one after another. Example 1, 2, 3, 4, 5, etc.
+        //  launch 1000 coroutines, Invoke doWork(index/number of coroutine) one after another. Example 1, 2, 3, 4, 5, etc.
 
-
-
-            CoroutineScope(IO).launch {
-
-                repeat(1000) {
-                  launch{
-                        list.add(it.toString())
-
+/*
+Dispatchers.Main-starts in main Thread,perform the UI operations within the coroutine
+ */
+                repeat(1000)
+                    {
+                    lifecycleScope.launch(Dispatchers.Main){
+                        doWork((it+1).toString())
                     }
-                    delay(300)
-                    doWork(list[it])
+
                 }
-
-            }
-
 
     }
 
     private fun runAsync() {
         println("runAsync method.")
+        //launch 1000 coroutines.Invoke doWork(index/number of coroutine) in async way. Example 1, 2, 5, 3, 4, 8, etc.
 
-        //  launch 1000 coroutines. Invoke doWork(index/number of coroutine) in async way. Example 1, 2, 5, 3, 4, 8, etc.
-        lifecycleScope.launch {
-
-            async(IO) {
-
-                repeat(1000) {
-                   async(IO){
-                        delay(500)
-                        list.add(it.toString())
-                    }
-
-                }
-
-                repeat(1000) {
-                    delay(1000)
-                    doWork(list[it])
-
-                }
-
-            }
-
-        }
+        /*
+        Planning to do Complex and long-running calculations,
+        which can block the main thread and freeze the UI.
+        It uses a shared background pool of threads
+          */
+                repeat(1000){
+                      lifecycleScope.launch(Dispatchers.Default) {
+                          doWork((it+1).toString())
+                      }
+                  }
     }
-
     private suspend fun doWork(name: String) {
         delay(500)
         state.update { "$name completed." }
